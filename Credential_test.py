419e8157efbe7fc11f3559eae6950758b53a468a
tumaa <ramanfatuu@gmail.com>
2019-10-06 13:29:20 +0300

    create test method for saving and initialization of the user object

---
+++ Credential_test.py
@@ -0,0 +1,32 @@
+import unittest
+from Credential import User
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        '''
+        this is setup method to run before each testcase
+        '''
+        self.new_user = User("tumaa", "1234ali")
+        
+    def tearDown(self):
+        User.user_list = []
+        
+    def test__init__(self):
+        '''
+        method to test if object is well initialized 
+        '''
+        self.assertEqual(self.new_user.lastname,"tumaa")
+        self.assertEqual(self.new_user.password,"1234ali")
+    
+    def test_save_user(self):
+        '''
+        method to test if the user object is saved into the user_list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
+        
